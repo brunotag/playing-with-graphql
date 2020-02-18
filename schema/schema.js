007d9a8c107c48b84cdf58bdbfc9eff6a670a4ed
Bruno Tagliapietra <bruno.tagliapietra@gmail.com>
2020-02-19 12:36:59 +1300

    add authors

--- schema/schema.js
+++ schema/schema.js
@@ -5,7 +5,8 @@ const {
     GraphQLObjectType, 
     GraphQLString, 
     GraphQLSchema,
-    GraphQLID
+    GraphQLID,
+    GraphQLInt
 } = graphql;
 
 const BookType = new GraphQLObjectType({
@@ -17,11 +18,30 @@ const BookType = new GraphQLObjectType({
     })
 });
 
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
+    })
+});
+
+// ** DUMMY DATA - BEGIN **
+
 var dummyBooks = [
     { name: 'Via col vento', genre: 'Pesanton', id: '1'},
     { name: 'Il codice Da Vinci', genre: 'Trash', id: '2'},
     { name: 'Pinocchio', genre: 'Scuola', id: '3'},
 ]
+
+var dummyAuthors = [
+    { name: 'Mario Rossi', age: 33, id: '1'},
+    { name: 'Carlo Verdi', age: 44, id: '2'},
+    { name: 'Luigi Bianchi', age: 55, id: '1'},
+]
+
+// ** DUMMY DATA - END **
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
@@ -33,6 +53,15 @@ const RootQuery = new GraphQLObjectType({
             },
             resolve(parent, args){                
                 return _.find(dummyBooks, { id: args.id });
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: {
+                id: { type: GraphQLID }
+            },
+            resolve(parent, args){                
+                return _.find(dummyAuthors, { id: args.id });
             }
         }
     }
