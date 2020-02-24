8a488bc322c96d84f39b7963a1a8672361be7f1f
Bruno Tagliapietra <bruno.tagliapietra@xerol.com>
2020-02-24 18:18:31 +1300

    mutations: addAuthor, addBook

--- schema/schema.js
+++ schema/schema.js
@@ -2,8 +2,8 @@ const graphql = require('graphql');
 
 const model = require('../model/model');
 const sequelize = require('../sql/connections').normalUserConnection;
-const Author = model.Author(sequelize);
-const Book = model.Book(sequelize);
+const SqlAuthor = model.Author(sequelize);
+const SqlBook = model.Book(sequelize);
 
 const {
     GraphQLObjectType, 
@@ -11,7 +11,8 @@ const {
     GraphQLSchema,
     GraphQLID,
     GraphQLInt,
-    GraphQLList
+    GraphQLList,
+    GraphQLNonNull
 } = graphql;
 
 const BookType = new GraphQLObjectType({
@@ -22,7 +23,7 @@ const BookType = new GraphQLObjectType({
         genre: { type: GraphQLString },
         author: {
             type: AuthorType,
-            resolve: (parent, args) => Author.findByPk(parent.author_id).then(author => author)
+            resolve: (parent, args) => SqlAuthor.findByPk(parent.author_id).then(author => author)
         }
     })
 });
@@ -36,7 +37,7 @@ const AuthorType = new GraphQLObjectType({
         books: {
             type: new GraphQLList(BookType),
             resolve(parent, args){
-                return Book.findAll({where: { author_id: parent.id} });
+                return SqlBook.findAll({where: { author_id: parent.id} });
             }
         }
     })
@@ -50,26 +51,59 @@ const RootQuery = new GraphQLObjectType({
             args: {
                 id: { type: GraphQLID }
             },
-            resolve: (parent, args) => Book.findByPk(args.id).then(book => book)
+            resolve: (parent, args) => SqlBook.findByPk(args.id).then(book => book)
         },
         author: {
             type: AuthorType,
             args: {
                 id: { type: GraphQLID }
             },
-            resolve: (parent, args) => Author.findByPk(args.id).then(author => author)
+            resolve: (parent, args) => SqlAuthor.findByPk(args.id).then(author => author)
         },
         books: {
             type: new GraphQLList(BookType),
-            resolve: (parent, args) => Book.findAll().then(books => books)
+            resolve: (parent, args) => SqlBook.findAll().then(books => books)
         },
         authors: {
             type: new GraphQLList(AuthorType),
-            resolve: (parent, args) => Author.findAll().then(authors => authors)
+            resolve: (parent, args) => SqlAuthor.findAll().then(authors => authors)
         }        
     }
 });
 
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: GraphQLInt }
+            },
+            resolve: (parent, args) =>  
+                SqlAuthor.create({                    
+                    name: args.name,
+                    age: args.age
+                }).then(author => author)            
+        },
+        addBook: {
+            type: BookType,
+            args: {
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                genre: {type: new GraphQLNonNull(GraphQLString)},
+                author_id: {type: new GraphQLNonNull(GraphQLID)} 
+            },
+            resolve: (parent, args) =>  
+                SqlBook.create({                    
+                    name: args.name,
+                    genre: args.genre,
+                    author_id: args.author_id
+                }).then(book => book)
+        }
+    }
+})
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 });
