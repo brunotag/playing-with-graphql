3d19a0671afc0c0aa5cf9818470a4443dfcb042d
Bruno Tagliapietra <bruno.tagliapietra@gmail.com>
2020-02-27 17:27:30 +1300

    create add book (doesn't work yet)
    show list of authors

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import BookList from './components/BookList'
-import ApolloClient from 'apollo-boost'
-import {ApolloProvider} from 'react-apollo'
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
+
+import BookList from './components/BookList';
+import AddBook from './components/AddBook';
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql"
 })
+
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <div id="main">
         <h1>My Reading List</h1>
-        <BookList></BookList>
+        <BookList/>
+        <AddBook/>
       </div>
     </ApolloProvider>
   );
