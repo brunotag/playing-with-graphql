cdfe49cbd152af839b397f833091c5d3561d01ce
Bruno Tagliapietra <bruno.tagliapietra@gmail.com>
2020-02-27 17:05:50 +1300

    show list of books

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {gql} from 'apollo-boost';
+import {graphql} from 'react-apollo'
 
-function BookList() {
-  return (
-    <div>
-      <ul id="book-list">
-        <li>Book name 1</li>
-        <li>Book name 2</li>
-        <li>Book name 3</li>
-      </ul>
-    </div>
-  );
+const getBooksQuery = gql`
+  {
+    books{
+      name
+      id
+    }
+  }
+`
+
+class BookList extends Component {
+  displayBooks() {
+    var data = this.props.data;
+    if (data.loading) {
+      return(<div>Loading books...</div>);
+    } else {
+      return data.books.map(book => {
+        return (
+          <li key={book.id}>{book.name}</li>
+        )
+      });
+    }
+  }
+
+  render() {
+    console.log(this.props)
+    return (
+      <div>
+        <ul id="book-list">
+          {this.displayBooks()}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
